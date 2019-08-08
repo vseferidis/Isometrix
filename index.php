534e628a5e04e99b7711570ea2c6cf20a5aefee1
Victor S. Seferidis <seferidis@outlook.com>
2019-08-08 20:16:27 +0200

    as

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/line-awesome.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body class="">
+    <!-- header area -->
+    <header id="header" class="header ">
+    <?php
+        include 'html/header.html';
+		?>
+		</header>
+    <main class="main ">
+        <div class="row main-wrapper">
+
+        </div>
+    </main>
+
+    <!-- footer area -->
+    <footer id="footer" class="footer"> 
+    <?php
+        include 'html/footer.html';
+		?>
+		</footer>
+
+
+
+
+
+		
+    <!-- Javascript Yard -->
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/js.js"></script>
+
+</body>
+
+</html>
--- js/js.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.get("header.html", function(data) {
-        $("#header").html(data);
-    });
-    $.get("footer.html", function(data) {
-        $("#footer").html(data);
-    });
-});
